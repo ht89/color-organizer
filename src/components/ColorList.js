46cd1958a12e4ceabd90e4ff9d36fd48f6efab15
Hung Tran <hungtran@bbv.ch>
2018-10-10 17:17:29 +0700

    commit

--- src/components/ColorList.js
+++ src/components/ColorList.js
@@ -1,35 +1,22 @@
 import PropTypes from 'prop-types'
 import Color from './Color'
 import '../../stylesheets/ColorList.scss'
-import { rateColor, removeColor } from '../actions';
-import { sortFunction } from '../lib/array-helpers';
 
-const ColorList = (props, { store }) => {
-    const { colors, sort } = store.getState()
+const ColorList = ({ colors = [] }) =>
+    <div className="color-list">
+        {(colors.length === 0) ?
+            <p>No Colors Listed. (Add a Color)</p> :
+            colors.map(color =>
+                <Color key={color.id}
+                    {...color}
+                />
+            )
+        }
+    </div>
 
-    const sortedColors = [...colors].sort(sortFunction(sort))
-
-    return (
-        <div className="color-list">
-            {(colors.length === 0) ?
-                <p>No Colors Listed. (Add a Color)</p> :
-                sortedColors.map(color =>
-                    <Color key={color.id}
-                        {...color}
-                        onRate={(rating) => store.dispatch(rateColor(color.id, rating))}
-                        onRemove={() => store.dispatch(removeColor(color.id))} />
-                )
-            }
-        </div>
-    )
-}
-
-ColorList.contextTypes = {
-    store: PropTypes.object
-}
 
 ColorList.propTypes = {
-    store: PropTypes.object
+    colors: PropTypes.array
 }
 
 export default ColorList
