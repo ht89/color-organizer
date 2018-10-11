1bb23402024683232e62f556cd362a61d5ca5e2e
Hung Tran <hungtran@bbv.ch>
2018-10-11 11:41:10 +0700

    commit

--- src/__tests__/store/reducers/color.test.js
+++ src/__tests__/store/reducers/color.test.js
@@ -24,5 +24,30 @@ describe('color Reducer', () => {
             })
     })
 
-    // it('RATE_COLOR success')
+    it('RATE_COLOR success', () => {
+        const state = {
+            id: 0,
+            title: 'Test Teal',
+            color: '#90C3C4',
+            timestamp: 'Sat Mar 12 2016 16:12:09 GMT-0800',
+            rating: undefined
+        }
+
+        const action = {
+            type: C.RATE_COLOR,
+            id: 0,
+            rating: 3
+        }
+
+        const results = color(state, action)
+
+        expect(results)
+            .toEqual({
+                id: action.id,
+                title: state.title,
+                color: state.color,
+                timestamp: state.timestamp,
+                rating: action.rating
+            })
+    })
 })
