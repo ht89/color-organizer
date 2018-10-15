7482fde5d006dfce2d71f27f3961d189c8a75b55
Hung Tran <hungtran@bbv.ch>
2018-10-15 14:32:51 +0700

    commit

--- src/components/ui/Color.js
+++ src/components/ui/Color.js
@@ -5,6 +5,34 @@ import { FaTrash } from 'react-icons/fa';
 import '../../../stylesheets/Color.scss'
 
 class Color extends Component {
+    componentWillMount() {
+        this.style = { backgroundColor: '#CCC' };
+    }
+
+    componentWillUpdate(nextProps) {
+        this.style = null;
+
+        const { title, rating } = this.props;
+        
+        this.refs.title.style.backgroundColor = 'red';
+        this.refs.title.style.color = 'white';
+
+        alert(`${title}: rating ${rating} -> ${nextProps.rating}`)
+    }
+
+    // improve performance by only updating the component where the rating has changed
+    shouldComponentUpdate(nextProps) {
+        const { rating } = this.props;
+        return rating !== nextProps.rating;
+    }
+
+    componentDidUpdate(prevProps) {
+        const { title, rating } = this.props;
+        const status = (rating > prevProps.rating) ? 'better' : 'worse';
+        console.log(`${title} is getting ${status}`);
+
+    }
+
     render() {
         const { id, title, color, rating, timestamp, onRemove, onRate } = this.props
 
